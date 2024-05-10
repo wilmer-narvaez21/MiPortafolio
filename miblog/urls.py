b6365959793913b2c0e9191163d497f558735321
Wilmer Rafael Narvaez <67441490+wilmer-narvaez21@users.noreply.github.com>
2024-05-09 23:36:26 -0600

    finalizacion de projecto
    terminamos la base del proyecto pero aun falta mejoras

--- miblog/urls.py
+++ miblog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import render_posts
+from .views import render_posts, post_detail
+
+# url que se identifican con un nombre esto lo podemos hacer con django con esta variable
+app_name = 'miblog'
 
 urlpatterns = [
-    path('', render_posts, name='posts'),
+    path('', render_posts, name="posts"),
+    path('/<int:post_id>', post_detail, name="post_detail"),
 ]
--- miblog/views.py
+++ miblog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Posts
 
 
 def render_posts(request):
-    return render(request, 'posts.html')
+    posts = Posts.objects.all()
+    return render(request, 'posts.html', {'posts': posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Posts, pk=post_id)
+    return render(request, 'post_detail.html', {"post": post})
